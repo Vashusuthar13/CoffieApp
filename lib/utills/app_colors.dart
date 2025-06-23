b58233dda63771896a775daa7f2dd6cbacf2a4ac
vashu <vashusuthar13@gmail.com>
2025-06-24 02:11:33 +0530

    app colors, packages, fontTheme

---
+++ lib/utills/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+
+  static const Color primary = Color(0xffC67C4E);
+  static const Color lightbrown = Color(0xffEDD6C8);
+  static const Color black = Color(0xff313131);
+  static const Color lightgrey = Color(0xffE3E3E3);
+  static const Color light = Color(0xffF9F2ED);
+
+
+}
